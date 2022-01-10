f82f2dd7f129b11d18a3bdab70b06d3a79923c25
Amy-Oji <amyojiakor@gmail.com>
2022-01-10 09:35:31 +0100

    classWorkStarted

---
+++ src/main/java/com/example/poddclasswork/PodDClassWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.poddclasswork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodDClassWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PodDClassWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/poddclasswork/PodDClassWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.poddclasswork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodDClassWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
